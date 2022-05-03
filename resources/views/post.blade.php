e9d72dae4a5826f312cd3719b6d1ac742c7fe4dd
neeckola <16673248+beraus@users.noreply.github.com>
2022-05-03 14:15:29 +0200

    ep25

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -1,7 +1,8 @@
   <x-layout>
       <article>
         <h1>{{ $post->title }}</h1>
-        <div>{{ $post->body }}</div>
+        <p><a href="/categories/{{ $post->category->slug }}">{{ $post->category->name }}</a></p>
+        <div>{{!! $post->body !!}}</div>
     </article>
 
     <a href="/">Go back</a>
