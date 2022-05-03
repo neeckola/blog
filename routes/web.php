1afaae02179c311fda6f7737865de45b04198991
neeckola <16673248+beraus@users.noreply.github.com>
2022-05-03 13:53:41 +0200

    ep23

--- routes/web.php
+++ routes/web.php
@@ -9,8 +9,8 @@ Route::get('/', function () {
     ]);
 });
 
-Route::get('posts/{post}', function($slug) {
+Route::get('posts/{post}', function(Post $post) {
     return view('post', [
-        'post' => Post::findOrFail($slug)
+        'post' => $post
     ]);
 });
