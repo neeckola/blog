4f371a17a72e0476cde5133744988ca31ff57ae6
neeckola <16673248+beraus@users.noreply.github.com>
2022-05-03 13:16:40 +0200

    ep20

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -2,27 +2,10 @@
 
 namespace App\Models;
 
-use Illuminate\Support\Facades\File;
-use Illuminate\Database\Eloquent\ModelNotFoundException;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
 
-class Post {
-    public static function find($slug) {
-        return static:all()->firstWhere('slug, $slug');    
-    }
-
-    public static function findOrFail($slug) {
-        $post = static:find($slug);
-
-        if(!$post) {
-            throw new ModelNotFoundException();
-        }
-
-        return $post;
-    }
-
-    public static function all() {
-        $files = File::files(resource_path("posts/"));
-
-        return array_map(fn($file) => $file->getContents(), $files);
-    }
+class Post extends Model
+{
+    use HasFactory;
 }
