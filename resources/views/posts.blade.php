3697a952dc2a27c810b4d69512b0816be34ac13d
neeckola <16673248+beraus@users.noreply.github.com>
2022-05-03 10:09:28 +0200

    ep 7

---
+++ resources/views/posts.blade.php
@@ -0,0 +1,24 @@
+<!doctype html>
+
+<title>Blog</title>
+<link rel="stylesheet" href="/app.css">
+
+<body>
+    <article>
+        <h1><a href="/post">my first post</a></h1>
+        
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis, debitis. Et nostrum natus aliquid neque obcaecati aperiam quas vitae deserunt perferendis est eveniet perspiciatis odit nihil, laudantium dolorem molestias aliquam!</p>
+    </article>
+
+    <article>
+        <h1>my 2nd post</h1>
+        
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis, debitis. Et nostrum natus aliquid neque obcaecati aperiam quas vitae deserunt perferendis est eveniet perspiciatis odit nihil, laudantium dolorem molestias aliquam!</p>
+    </article>
+
+    <article>
+        <h1>my 3rd post</h1>
+        
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis, debitis. Et nostrum natus aliquid neque obcaecati aperiam quas vitae deserunt perferendis est eveniet perspiciatis odit nihil, laudantium dolorem molestias aliquam!</p>
+    </article>
+</body>
