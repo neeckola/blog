570707a9b942aecab5b8a2b19e0c9200323a4920
neeckola <16673248+beraus@users.noreply.github.com>
2022-05-03 10:48:09 +0200

    e11

---
+++ resources/views/post.blade.php
@@ -0,0 +1,14 @@
+<!doctype html>
+
+<title>Blog</title>
+<link rel="stylesheet" href="/app.css">
+
+<body>
+    <article>
+        <h1><a href="/post">my first post</a></h1>
+        
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis, debitis. Et nostrum natus aliquid neque obcaecati aperiam quas vitae deserunt perferendis est eveniet perspiciatis odit nihil, laudantium dolorem molestias aliquam!</p>
+    </article>
+
+    <a href="/">Go back</a>
+</body>
--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -5,19 +5,19 @@
 
 <body>
     <article>
-        <h1><a href="/post">my first post</a></h1>
+        <h1><a href="/posts/my-first-post">my first post</a></h1>
         
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis, debitis. Et nostrum natus aliquid neque obcaecati aperiam quas vitae deserunt perferendis est eveniet perspiciatis odit nihil, laudantium dolorem molestias aliquam!</p>
     </article>
 
     <article>
-        <h1>my 2nd post</h1>
+        <h1><a href="/posts/my-second-post">my 2nd post</a></h1>
         
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis, debitis. Et nostrum natus aliquid neque obcaecati aperiam quas vitae deserunt perferendis est eveniet perspiciatis odit nihil, laudantium dolorem molestias aliquam!</p>
     </article>
 
     <article>
-        <h1>my 3rd post</h1>
+        <h1><a href="/posts/my-third-post">my 3rd post</a></h1>
         
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis, debitis. Et nostrum natus aliquid neque obcaecati aperiam quas vitae deserunt perferendis est eveniet perspiciatis odit nihil, laudantium dolorem molestias aliquam!</p>
     </article>
