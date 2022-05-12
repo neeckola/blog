636c0a81fc6c2f138ce699fa99931c592a66b6f9
neeckola <16673248+beraus@users.noreply.github.com>
2022-05-12 14:01:46 +0200

    ep40

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -16,9 +16,14 @@ class Post extends Model
     protected $with = ['category', 'author'];
 
     public function scopeFilter($query, array $filters) {
-        if($filters['search'] ?? false) {
-            $query->where('title', 'like', '%' . request('search') . '%')->orWhere('body', 'like', '%' . request('search') . '%');
-        }
+        $query->when($filters['search'] ?? false, fn($query, $search) =>
+            $query->where('title', 'like', '%' . request('search') . '%')
+                ->orWhere('body', 'like', '%' . request('search') . '%'));
+        
+        $query->when($filters['category'] ?? false, fn($query, $category) =>
+            $query->whereHas('category', fn($query) => $query->where('slug', $category))
+        );
+
     }
 
     public function category() {
